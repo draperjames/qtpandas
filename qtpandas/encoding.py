de54485d290cdfb2922f3141fcd23a91b88d30e8
James Draper <james.draper@duke.edu>
2016-12-08 09:41:23 -0500

    Let's call this version 1.0.0.
    I think we can say that we are out of the beta version. I added a
    codacity badge and made a few minor changes to improve that score.

--- qtpandas/encoding.py
+++ qtpandas/encoding.py
@@ -27,10 +27,12 @@ class Detector(object):
     def __init__(self):
         if AUTODETECT:
             if sys.platform.startswith('linux'):
-                # use the system wide installed version comming with windows, the included magic.mgc might be incompatible
+                # Use the system wide installed version comming with windows.
+                # The included magic.mgc might be incompatible.
                 magic_db = os.path.join('/usr/share/file', 'magic.mgc')
             else:
-                magic_db = os.path.join(BASEDIR, '_lib', 'magic', 'db', 'magic.mgc')
+                magic_db = os.path.join(BASEDIR, '_lib', 'magic', 'db',
+                                        'magic.mgc')
             if not os.path.exists(magic_db):
                 raise ImportError('Please install libmagic.')
             self.magic = magic.Magic(magic_file=magic_db, mime_encoding=True)
