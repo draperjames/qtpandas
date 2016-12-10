4bea60eb4dc801ac2d166b01c9ddf1fd2a12ff69
Zeke <zekebarge@gmail.com>
2016-12-10 01:55:18 -0800

    Started imports of main features in the home __init__ so we can easily import them just like in pandas ;)

--- qtpandas/__init__.py
+++ qtpandas/__init__.py
@@ -1,3 +1,10 @@
 # -*- coding: utf-8 -*-
 # __import__('pkg_resources').declare_namespace(__name__)
 __version__ = '1.0.2'
+from qtpandas.models.DataFrameModel import DataFrameModel
+from qtpandas.models.ColumnDtypeModel import ColumnDtypeModel
+from qtpandas.models.DataSearch import DataSearch
+from qtpandas.utils import superReadFile, superReadCSV, superReadText
+from qtpandas.views.CSVDialogs import CSVExportDialog, CSVImportDialog
+from qtpandas.views.EditDialogs import AddAttributesDialog, RemoveAttributesDialog
+
