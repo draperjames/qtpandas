3623e94be46451d6a4d2ed14571d5807ab6ef4fb
James Draper <james.draper@duke.edu>
2016-12-18 12:05:39 -0500

    Changing 'bool' to bool and a few PEP8 tweaks
    I realized that I changed 'QBool' to 'bool' when it probably should have
    been just bool.

--- qtpandas/__init__.py
+++ qtpandas/__init__.py
@@ -1,8 +1,19 @@
 # -*- coding: utf-8 -*-
-__import__('pkg_resources').declare_namespace(__name__)
-__version__ = '1.0.4'
-from qtpandas.models.DataFrameModel import DataFrameModel, read_file, read_sql
+
+from qtpandas.models.DataFrameModel import (DataFrameModel,
+                                            read_file,
+                                            read_sql)
+
 from qtpandas.models.ColumnDtypeModel import ColumnDtypeModel
+
 from qtpandas.models.DataSearch import DataSearch
-from qtpandas.views.CSVDialogs import CSVExportDialog, CSVImportDialog
-from qtpandas.views.EditDialogs import AddAttributesDialog, RemoveAttributesDialog
+
+from qtpandas.views.CSVDialogs import (CSVExportDialog,
+                                       CSVImportDialog)
+
+from qtpandas.views.EditDialogs import (AddAttributesDialog,
+                                        RemoveAttributesDialog)
+
+# __import__('pkg_resources').declare_namespace(__name__)
+
+__version__ = '1.0.4'
